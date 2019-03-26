4d97c1943096b2f58c5324a812276c1801e64e90
WaysideTester <dylanmurray211@outlook.com>
2019-03-25 23:40:10 -0500

    completed variable data input Naive Bayes function

--- NaiveBayes/Program.cs
+++ NaiveBayes/Program.cs
@@ -23,41 +23,17 @@ namespace NaiveBayes
             info.Add(new Data("female", 5.4, 130, 7));
             info.Add(new Data("female", 5.75, 150, 9));
 
-            List<double> TrainData(List<Data> trainingData)
+            void TrainData(List<Data> trainingData)
             {
                 // separates the training data by what is being solved for
                 IEnumerable<IGrouping<string, Data>> groupedTrainingData = WhatToSolveFor(trainingData);
 
-                LoopOverData(groupedTrainingData, trainingData.Count);
+                var globalDictionary = LoopOverData(groupedTrainingData, trainingData.Count);
 
-                double firstAvg = trainingData.Select(t => t.Height).ToList().Average();
-                double secondAvg = trainingData.Select(t => t.Weight).ToList().Average();
-                double thirdAvg = trainingData.Select(t => t.Foot).ToList().Average();
-
-                double firstVari = Variance(firstAvg, trainingData.Select(t => t.Height).ToList());
-                double secondVari = Variance(firstAvg, trainingData.Select(t => t.Weight).ToList());
-                double thirdVari = Variance(firstAvg, trainingData.Select(t => t.Foot).ToList());
-
-                var PredictionOutputList = trainingData.Select(t => t.Gender)
-                    .GroupBy(str => str, (key, list) => new { Type = key, Count = list.Count() }).ToList();
-
-                double predictOutputA = PredictionOutputList[0].Count/(PredictionOutputList[0].Count + PredictionOutputList[1].Count);
-                double predictOutputB = PredictionOutputList[1].Count/(PredictionOutputList[0].Count + PredictionOutputList[1].Count);
-
-                return new List<double>
-                {
-                    firstAvg,
-                    firstVari,
-                    secondAvg,
-                    secondVari,
-                    thirdAvg,
-                    thirdVari,
-                    predictOutputA,
-                    predictOutputB,
-                };
+                Finalize(globalDictionary, new WhatToSolveFor(6, 130, 8));
             }
 
-            void LoopOverData(IEnumerable<IGrouping<string, Data>> groupedData, double totalTrainingData)
+            Dictionary<string,Dictionary<string,Dictionary<string, double>>> LoopOverData(IEnumerable<IGrouping<string, Data>> groupedData, double totalTrainingData)
             {
                 // outcome is key, dictionary of props is value
                 var allDataDictionary = new Dictionary<string, Dictionary<string, Dictionary<string, double>>>();
@@ -115,7 +91,7 @@ namespace NaiveBayes
                     } // =============== End Loop on properties ===============
                     allDataDictionary.Add(groupedList.Key, outcomeDictionary);
                 } // =============== End Loop on groupedData ===============
-
+                return allDataDictionary;
             } // =============== End LoopOverData Method ===============
 
             IEnumerable<IGrouping<string, Data>> WhatToSolveFor(List<Data> trainingList)
@@ -124,68 +100,83 @@ namespace NaiveBayes
                 return groupedTrainingData;
             }
 
-            List<double> predictorConstants = TrainData(info);
-
-            double maleCount = 0;
-            List<double> maleHeight = new List<double>();
-            List<double> maleWeight = new List<double>();
-            List<double> maleFoot = new List<double>();
-            double femaleCount = 0;
-            List<double> femaleHeight = new List<double>();
-            List<double> femaleWeight = new List<double>();
-            List<double> femaleFoot = new List<double>();
-
-
-            for (int i = 0; i < info.Count; i++)
-            {
-                var data = info[i];
-                if (data.Gender == "male")
-                {
-                    maleHeight.Add(data.Height);
-                    maleWeight.Add(data.Weight);
-                    maleFoot.Add(data.Foot);
-                    maleCount++;
-                }
-                else if (data.Gender == "female")
-                {
-                    femaleHeight.Add(data.Height);
-                    femaleWeight.Add(data.Weight);
-                    femaleFoot.Add(data.Foot);
-                    femaleCount++;
-                }
-            }
-
             double Variance(double average, List<double>listOfMeasurement)
             {
                 return (listOfMeasurement.Select(x => Math.Pow(x - average, 2)).Sum() / (listOfMeasurement.Count - 1));
             }
 
-            /* double Average(List<Data> trainingData, PropertyInfo propSelector)
-            {
-                return trainingData.Select(t => new Double(d).ToList().Average();
-            } */
-
-            var maleHeightAve = maleHeight.Average();
-            var maleHeightVari = Variance(maleHeightAve, maleHeight);
-            var maleWeightAve = maleWeight.Average();
-            var maleWeightVari = Variance(maleWeightAve, maleWeight);
-            var maleFootAve = maleFoot.Average();
-            var maleFootVari = Variance(maleFootAve, maleFoot);
-
-            Console.WriteLine($"{maleHeightAve} + {maleWeightAve} + {maleFootAve}");
-            Console.WriteLine($"{maleHeightVari} + {maleWeightVari} + {maleFootVari}");
-
-            double SingleProbability(double newMeasure, double testMeasureAverage, double testMeasureVariance)
+             double SingleProbability(double newMeasure, double testMeasureAverage, double testMeasureVariance)
             {
                 return ((1/(Math.Sqrt(2*Math.PI*testMeasureVariance))) * Math.Pow(Math.E, ((-1 * Math.Pow(newMeasure - testMeasureAverage, 2)) / (2 * testMeasureVariance))));
             }
 
-            var maleProbHeight = SingleProbability(6, maleHeightAve, maleHeightVari);
-            var maleProbWeight = SingleProbability(130, maleWeightAve, maleWeightVari);
-            var maleProbFoot = SingleProbability(8, maleFootAve, maleFootVari);
-            var maleProbGender = (maleCount / (maleCount + femaleCount));
+            void Finalize(Dictionary<string, Dictionary<string, Dictionary<string, double>>> trainingDictionary,  WhatToSolveFor predictionData)
+            {
+                // List to give probability in %
+                var finalOutcomeProbabilities = new List<Tuple<string, double>>();
 
-            Console.WriteLine(maleProbFoot * maleProbGender * maleProbHeight * maleProbWeight);
+                // ================== Each outcome to check ==================
+                foreach (var (valToSolveFor, propList) in trainingDictionary)
+                {
+                    // 1 is neutral number to multiply by for probability function
+                    double finalProbforVal = 1;
+
+                    // ================== loop over properteis and output probability per property ==================
+                    foreach (var (propName, propOutput) in propList)
+                    {
+                        // newMeasure will be set to the current property as long as it was given in the prediction data
+                        double newMeasure = 0;
+                        
+                        // If the property doesn't exist in prediction data, it is what is being solved for. Here, we skip over it if it doesn't exist
+                        if (typeof(WhatToSolveFor).GetProperty(propName) != null)
+                        {
+                            // extracts the value from the given property based on the variable property name
+                            newMeasure = Convert.ToDouble(predictionData.GetType().GetProperty(propName).GetValue(predictionData));
+                        }
+
+                        // creates variables to be set later
+                        double average = 0;
+                        double variance = 0;
+
+                        // ================== Loops over property values for extraction ==================
+                        foreach (var (type, number) in propOutput)
+                        {
+                            // checks if it is categorical. Passes probability to finalPobforVal equation
+                            if(type == "probability")
+                            {
+                                finalProbforVal = number * finalProbforVal;
+                            }
+
+                            // These Else-Ifs set the average and variance to be passed to a probability funciton that takes in the prediction data
+                            else if(type == "average")
+                            {
+                                average = number;
+                            }
+                            else if (type == "variance")
+                            {
+                                variance = number;
+                            }
+                        }
+
+                        // if propOutput.Count is > 1, it has an average and variance. If not, It has already taken care of this step
+                        if(propOutput.Count > 1)
+                        {
+                            finalProbforVal = SingleProbability(newMeasure, average, variance) * finalProbforVal;
+                        }
+                    }
+                    // Adds the probability in a number and what the likely outcome is to a list
+                    finalOutcomeProbabilities.Add(new Tuple<string,double>(valToSolveFor, finalProbforVal));
+                }
+
+                // Loops over possible outcomes and prints to the console the % probability that outcome will occur.
+                foreach (var outcome in finalOutcomeProbabilities)
+                {
+                    Console.WriteLine($"Percent Likely it is {outcome.Item1}: {(outcome.Item2 / (finalOutcomeProbabilities.Sum(x => x.Item2)))*100}%");
+                }
+            }
+
+            // Executes the entire set of instructions listed above
+            TrainData(info);
 
             Console.ReadKey();
         }
